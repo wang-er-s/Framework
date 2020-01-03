59276b119837adddb166bd997490133544220439
zhangguoju <zhangguoju@longtugame.com>
2020-01-03 20:33:53 +0800

    eee

--- AD/Resources/Configs.cs
+++ AD/Resources/Configs.cs
@@ -9,9 +9,9 @@ public static class Configs
     public const string EditorResourcesDir = "ResPackage";
     public const string BundlesDirName = "Bundles"; //---
     public const string EditorAssetBundlePath = "";
-    public const bool LoadAssetBundle = true;
-    public const bool ApplicationQuited = false;
-    public const bool IsEditor = false;
+    public static bool LoadAssetBundle = true;
+    public static bool ApplicationQuited = false;
+    public static bool IsEditor = false;
     /// <summary>
     /// 是否優先找下載的資源?還是app本身資源優先. 优先下载的资源，即采用热更新的资源
     /// </summary>
