ac31370c6b36b9f0e9b32269c43611710b84ef39
zhangguoju <18530117331@163.com>
2020-08-03 02:24:23 +0800

    更改文件目录

---
+++ Assets/Framework/Util/ScriptCollection/Empty4Raycast.cs
@@ -0,0 +1,22 @@
+using UnityEngine.UI;
+
+namespace Framework
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// reference from :http://gad.qq.com/article/detail/37846
+	/// 替换空 Image 扩大点击区域，降低 Drawcall。
+	/// </summary>
+	public class Empty4Raycast  : MaskableGraphic
+	{
+		protected Empty4Raycast()
+		{
+			useLegacyMeshGeneration = false;
+		}
+
+		protected override void OnPopulateMesh(VertexHelper toFill)
+		{
+			toFill.Clear();
+		}
+	}
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
