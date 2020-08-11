6ec0b3b732ac1f24447c0d0822d5ec891b307d29
zhangguoju <18530117331@163.com>
2020-08-12 01:52:08 +0800

    增加一些帮助类

--- Assets/Framework/CommonHelper/CommonHelper.cs
+++ Assets/Framework/CommonHelper/CommonHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -16,7 +17,107 @@ namespace Framework.CommonHelper
             Resources.UnloadUnusedAssets();
             GC.Collect();
         }
+        
+        /// <summary>
+        /// 网络可用
+        /// </summary>
+        public static bool NetAvailable => Application.internetReachability != NetworkReachability.NotReachable;
 
+        /// <summary>
+        /// 是否是无线
+        /// </summary>
+        public static bool IsWifi => Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork;
+
+        
+        /// <summary>
+        /// 获取当前运行的设备平台信息
+        /// </summary>
+        /// <returns></returns>
+        public static string GetDeviceInfo()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("Device And Sysinfo:\r\n");
+            stringBuilder.Append(string.Format("DeviceModel: {0} \r\n", SystemInfo.deviceModel));
+            stringBuilder.Append(string.Format("DeviceName: {0} \r\n", SystemInfo.deviceName));
+            stringBuilder.Append(string.Format("DeviceType: {0} \r\n", SystemInfo.deviceType));
+            stringBuilder.Append(string.Format("BatteryLevel: {0} \r\n", SystemInfo.batteryLevel));
+            stringBuilder.Append(string.Format("DeviceUniqueIdentifier: {0} \r\n", SystemInfo.deviceUniqueIdentifier));
+            stringBuilder.Append(string.Format("SystemMemorySize: {0} \r\n", SystemInfo.systemMemorySize));
+            stringBuilder.Append(string.Format("OperatingSystem: {0} \r\n", SystemInfo.operatingSystem));
+            stringBuilder.Append(string.Format("GraphicsDeviceID: {0} \r\n", SystemInfo.graphicsDeviceID));
+            stringBuilder.Append(string.Format("GraphicsDeviceName: {0} \r\n", SystemInfo.graphicsDeviceName));
+            stringBuilder.Append(string.Format("GraphicsDeviceType: {0} \r\n", SystemInfo.graphicsDeviceType));
+            stringBuilder.Append(string.Format("GraphicsDeviceVendor: {0} \r\n", SystemInfo.graphicsDeviceVendor));
+            stringBuilder.Append(string.Format("GraphicsDeviceVendorID: {0} \r\n", SystemInfo.graphicsDeviceVendorID));
+            stringBuilder.Append(string.Format("GraphicsDeviceVersion: {0} \r\n", SystemInfo.graphicsDeviceVersion));
+            stringBuilder.Append(string.Format("GraphicsMemorySize: {0} \r\n", SystemInfo.graphicsMemorySize));
+            stringBuilder.Append(string.Format("GraphicsMultiThreaded: {0} \r\n", SystemInfo.graphicsMultiThreaded));
+            stringBuilder.Append(string.Format("SupportedRenderTargetCount: {0} \r\n", SystemInfo.supportedRenderTargetCount));
+            return stringBuilder.ToString();
+        }
+        
+        private static bool? isIpad;
+
+        public static bool IsPad
+        {
+            get
+            {
+                if (isIpad != null) return isIpad.Value;
+                string type = SystemInfo.deviceModel.ToLower().Trim();
+                switch (type.Substring(0, 3))
+                {
+                    case "iph":
+                        //iPhone机型
+                        isIpad = false;
+                        break;
+                    case "ipa":
+                        //iPad机型
+                        isIpad = true;
+                        break;
+                    default:
+                        //其他
+                        isIpad = false;
+                        break;
+                }
+
+                return isIpad.Value;
+            }
+        }
+        
+        /// <summary>
+        /// 获取设备的电量
+        /// </summary>
+        /// <returns></returns>
+        public static float GetBatteryLevel()
+        {
+            if (Application.isMobilePlatform)
+            {
+                return SystemInfo.batteryLevel;
+            }
+
+            return 1;
+        }
+        
+        /// <summary>
+        /// 获取设备的电池状态
+        /// </summary>
+        /// <returns></returns>
+        public static BatteryStatus GetBatteryStatus()
+        {
+            return SystemInfo.batteryStatus;
+        }
+        
+        /// <summary>
+        /// 获取设备网络的状况
+        /// </summary>
+        /// <returns></returns>
+        public static NetworkReachability GetNetworkStatus()
+        {
+            return Application.internetReachability;
+        }
+        
+        
+        
         #region Editor
 
 #if UNITY_EDITOR
