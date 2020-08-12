dc47e5c8a81d663a0e4987bbbe39a45a74e3f350
zhangguoju <18530117331@163.com>
2020-08-12 20:15:26 +0800

    add audio

---
+++ Assets/Framework/Audio/EAudioLayer.cs
@@ -0,0 +1,28 @@
+﻿namespace Framework.Audio
+{
+	/// <summary>
+	/// 音频层级
+	/// </summary>
+	public enum EAudioLayer
+	{
+		/// <summary>
+		/// 音乐
+		/// </summary>
+		Music,
+
+		/// <summary>
+		/// 环境
+		/// </summary>
+		Ambient,
+
+		/// <summary>
+		/// 语音
+		/// </summary>
+		Voice,
+
+		/// <summary>
+		/// 音效
+		/// </summary>
+		Sound,
+	}
+}
