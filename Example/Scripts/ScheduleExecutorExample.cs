a9b0d828f1ae3c8df27eff97a77b8d2793927333
zhangguoju <18530117331@163.com>
2020-08-12 20:38:56 +0800

    增加案例

---
+++ Example/Scripts/ScheduleExecutorExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Framework.Execution;
+using UnityEngine;
+using IAsyncResult = Framework.Asynchronous.IAsyncResult;
+
+namespace Framework.Example
+{
+    public class ScheduleExecutorExample : MonoBehaviour
+    {
+        private void Start()
+        {
+            IScheduledExecutor scheduledExecutor = new ThreadScheduledExecutor();
+            scheduledExecutor.Start();
+            IAsyncResult asyncResult1 = null;
+            asyncResult1 = scheduledExecutor.ScheduleAtFixedRate(time =>
+            {
+                print(time);
+                if (time >= 5000)
+                    asyncResult1.Cancel();
+            }, () => print($"结束"), 1000, 500, 5000);
+            
+            
+            scheduledExecutor = new CoroutineScheduledExecutor();
+            scheduledExecutor.Start();
+            IAsyncResult asyncResult2 = null;
+            asyncResult2 = scheduledExecutor.ScheduleAtFixedRate(time =>
+            {
+                print(time);
+                if (time >= 5000)
+                    asyncResult2.Cancel();
+            }, () => print($"结束"), 1000, 500, 5000);
+        }
+    }
+}
