3c1482530294da92ad88aa50c620499ae2a4e6cc
zhangguoju <18530117331@163.com>
2020-08-12 20:38:56 +0800

    增加案例

---
+++ Assets/Framework/Example/Scripts/CoroutineTaskExample.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Text;
+using Framework.Asynchronous;
+using Framework.Execution;
+using UnityEngine;
+
+namespace Framework.Example
+{
+    public class CoroutineTaskExample : MonoBehaviour
+	{
+		IEnumerator Start()
+		{
+			CoroutineTask task = new CoroutineTask(DoTask())
+				.ContinueWith(
+					DoContinueTask(),
+					CoroutineTaskContinuationOptions.OnCompleted
+					| CoroutineTaskContinuationOptions.OnFaulted
+				).ContinueWith(
+					() => { Debug.Log("The task is completed"); }
+				);
+
+			yield return task.WaitForDone();
+
+			Debug.LogFormat("IsDone:{0} IsCompleted:{1} IsFaulted:{2} IsCancelled:{3}",
+				task.IsDone, task.IsCompleted, task.IsFaulted, task.IsCancelled);
+		}
+
+		/// <summary>
+		/// Simulate a task.
+		/// </summary>
+		/// <returns>The task.</returns>
+		/// <param name="promise">Promise.</param>
+		protected IEnumerator DoTask()
+		{
+			int n = 10;
+			for (int i = 0; i < n; i++)
+			{
+				Debug.LogFormat("Task:i = {0}", i);
+				yield return new WaitForSeconds(0.5f);
+			}
+		}
+
+		protected IEnumerator DoContinueTask()
+		{
+			int n = 10;
+			for (int i = 0; i < n; i++)
+			{
+				Debug.LogFormat("ContinueTask:i = {0}", i);
+				yield return new WaitForSeconds(0.5f);
+			}
+		}
+	}
+}
---
+++ Assets/Framework/Example/Scripts/ProgressResultCallbackExample.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using Framework.Asynchronous;
+using UnityEngine;
+
+namespace Framework.Example
+{
+    public class ProgressResultCallbackExample : MonoBehaviour
+    {
+        void Start()
+        {
+            ProgressResult<float, bool> result = new ProgressResult<float, bool>();
+
+            /* Register a callback */
+            result.Callbackable().OnProgressCallback(p => { Debug.LogFormat("Progress:{0}%", p * 100); });
+
+            result.Callbackable().OnCallback((r) =>
+            {
+                if (r.Exception != null)
+                {
+                    Debug.LogFormat("The task is finished.IsDone:{0} Exception:{1}", r.IsDone, r.Exception);
+                    return;
+                }
+
+                Debug.LogFormat("The task is finished. IsDone:{0} Result:{1}", r.IsDone, r.Result);
+            });
+
+            /* Start the task */
+            this.StartCoroutine(DoTask(result));
+        }
+
+        /// <summary>
+        /// Simulate a task.
+        /// </summary>
+        /// <returns>The task.</returns>
+        /// <param name="promise">Promise.</param>
+        protected IEnumerator DoTask(IProgressPromise<float, bool> promise)
+        {
+            int n = 50;
+            for (int i = 0; i < n; i++)
+            {
+                promise.UpdateProgress((float) i / n);
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            promise.SetResult(true);
+        }
+    }
+}
