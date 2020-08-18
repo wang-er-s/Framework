6ce027a4eb41dbd624f4eac213d1b5051b075796
zhangguoju <18530117331@163.com>
2020-08-18 14:50:51 +0800

    tag

--- Tag.cs
+++ Tag.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ss : MonoBehaviour
+public class Tag : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
