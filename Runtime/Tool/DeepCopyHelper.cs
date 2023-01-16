06d0f86c6ec3b68e44ba1b5a7583094025d02b56
zhangguoju <18530117331@163.com>
2023-01-16 16:20:24 +0800

    增加npbehave

---
+++ Runtime/Tool/DeepCopyHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+using CatJson;
+
+namespace Framework
+{
+    public static class DeepCloneHelper
+    {
+        /// <summary>
+        /// 深拷贝
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static T DeepCopy<T>(this T obj)
+            where T : class
+        {
+            try
+            {
+                if (obj == null)
+                {
+                    return null;
+                }
+
+                return JsonParser.Default.ParseJson<T>(JsonParser.Default.ToJson(obj));
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                return null;
+            }
+        }
+
+        public static T DeepCopyByReflect<T>(T obj)
+        {
+            //如果是字符串或值类型则直接返回
+            if (obj is string || obj.GetType().IsValueType) return obj;
+
+            object retval = Activator.CreateInstance(obj.GetType());
+            FieldInfo[] fields = obj.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            foreach (FieldInfo field in fields)
+            {
+                try { field.SetValue(retval, DeepCopyByReflect(field.GetValue(obj))); }
+                catch { }
+            }
+            return (T)retval;
+        }
+    }
+}
