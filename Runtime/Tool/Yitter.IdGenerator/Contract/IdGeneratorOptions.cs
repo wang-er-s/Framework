fec365929cd066a5aef2b983156514345dd227fe
zhangguoju <18530117331@163.com>
2023-01-29 17:07:38 +0800

    增加idgenerator

---
+++ Runtime/Tool/Yitter.IdGenerator/Contract/IdGeneratorOptions.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * 版权属于：yitter(yitter@126.com)
+ * 开源地址：https://github.com/yitter/idgenerator
+ * 版权协议：MIT
+ * 版权说明：只要保留本版权，你可以免费使用、修改、分发本代码。
+ * 免责条款：任何因为本代码产生的系统、法律、政治、宗教问题，均与版权所有者无关。
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework
+{
+    public class IdGeneratorOptions
+    {
+        /// <summary>
+        /// 雪花计算方法
+        /// （1-漂移算法|2-传统算法），默认1
+        /// </summary>
+        public virtual short Method { get; set; } = 1;
+
+        /// <summary>
+        /// 基础时间（UTC格式）
+        /// 不能超过当前系统时间
+        /// </summary>
+        public virtual DateTime BaseTime { get; set; } = new DateTime(2020, 2, 20, 2, 20, 2, 20, DateTimeKind.Utc);
+
+        /// <summary>
+        /// 机器码
+        /// 必须由外部设定，最大值 2^WorkerIdBitLength-1
+        /// </summary>
+        public virtual ushort WorkerId { get; set; } = 0;
+
+        /// <summary>
+        /// 机器码位长
+        /// 默认值6，取值范围 [1, 15]（要求：序列数位长+机器码位长不超过22）
+        /// </summary>
+        public virtual byte WorkerIdBitLength { get; set; } = 6;//10;
+
+        /// <summary>
+        /// 序列数位长
+        /// 默认值6，取值范围 [3, 21]（要求：序列数位长+机器码位长不超过22）
+        /// </summary>
+        public virtual byte SeqBitLength { get; set; } = 6;//10;
+
+        /// <summary>
+        /// 最大序列数（含）
+        /// 设置范围 [MinSeqNumber, 2^SeqBitLength-1]，默认值0，表示最大序列数取最大值（2^SeqBitLength-1]）
+        /// </summary>
+        public virtual int MaxSeqNumber { get; set; } = 0;
+
+        /// <summary>
+        /// 最小序列数（含）
+        /// 默认值5，取值范围 [5, MaxSeqNumber]，每毫秒的前5个序列数对应编号0-4是保留位，其中1-4是时间回拨相应预留位，0是手工新值预留位
+        /// </summary>
+        public virtual ushort MinSeqNumber { get; set; } = 5;
+
+        /// <summary>
+        /// 最大漂移次数（含），
+        /// 默认2000，推荐范围500-10000（与计算能力有关）
+        /// </summary>
+        public virtual int TopOverCostCount { get; set; } = 2000;
+
+        /// <summary>
+        /// 数据中心ID（默认0）
+        /// </summary>
+        public virtual uint DataCenterId { get; set; } = 0;
+
+        /// <summary>
+        /// 数据中心ID长度（默认0）
+        /// </summary>
+        public virtual byte DataCenterIdBitLength { get; set; } = 0;
+
+        /// <summary>
+        /// 时间戳类型（0-毫秒，1-秒），默认0
+        /// </summary>
+        public virtual byte TimestampType { get; set; } = 0;
+
+
+        public IdGeneratorOptions()
+        {
+
+        }
+
+        public IdGeneratorOptions(ushort workerId)
+        {
+            WorkerId = workerId;
+        }
+    }
+}
---
+++ Runtime/Tool/Yitter.IdGenerator/DefaultIdGenerator.cs
@@ -0,0 +1,130 @@
+﻿/*
+ * 版权属于：yitter(yitter@126.com)
+ * 开源地址：https://github.com/yitter/idgenerator
+ * 版权协议：MIT
+ * 版权说明：只要保留本版权，你可以免费使用、修改、分发本代码。
+ * 免责条款：任何因为本代码产生的系统、法律、政治、宗教问题，均与版权所有者无关。
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Framework
+{
+    /// <summary>
+    /// 默认实现
+    /// </summary>
+    public class DefaultIdGenerator : IIdGenerator
+    {
+        private ISnowWorker _SnowWorker { get; set; }
+
+        //public Action<OverCostActionArg> GenIdActionAsync
+        //{
+        //    get => _SnowWorker.GenAction;
+        //    set => _SnowWorker.GenAction = value;
+        //}
+
+
+        public DefaultIdGenerator(IdGeneratorOptions options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentException("options error.");
+            }
+
+            // 1.BaseTime
+            if (options.BaseTime < DateTime.Now.AddYears(-50) || options.BaseTime > DateTime.Now)
+            {
+                throw new ArgumentException("BaseTime error.");
+            }
+
+            // 2.WorkerIdBitLength
+            int maxLength = options.TimestampType == 0 ? 22 : 31; // （秒级时间戳时放大到31位）
+            if (options.WorkerIdBitLength <= 0)
+            {
+                throw new ArgumentException("WorkerIdBitLength error.(range:[1, 21])");
+            }
+            if (options.DataCenterIdBitLength + options.WorkerIdBitLength + options.SeqBitLength > maxLength)
+            {
+                throw new ArgumentException("error：DataCenterIdBitLength + WorkerIdBitLength + SeqBitLength <= " + maxLength);
+            }
+
+            // 3.WorkerId & DataCenterId
+            var maxWorkerIdNumber = (1 << options.WorkerIdBitLength) - 1;
+            if (maxWorkerIdNumber == 0)
+            {
+                maxWorkerIdNumber = 63;
+            }
+            if (options.WorkerId < 0 || options.WorkerId > maxWorkerIdNumber)
+            {
+                throw new ArgumentException("WorkerId error. (range:[0, " + maxWorkerIdNumber + "]");
+            }
+
+            var maxDataCenterIdNumber = (1 << options.DataCenterIdBitLength) - 1;
+            if (options.DataCenterId < 0 || options.DataCenterId > maxDataCenterIdNumber)
+            {
+                throw new ArgumentException("DataCenterId error. (range:[0, " + maxDataCenterIdNumber + "]");
+            }
+
+            // 4.SeqBitLength
+            if (options.SeqBitLength < 2 || options.SeqBitLength > 21)
+            {
+                throw new ArgumentException("SeqBitLength error. (range:[2, 21])");
+            }
+
+            // 5.MaxSeqNumber
+            var maxSeqNumber = (1 << options.SeqBitLength) - 1;
+            if (maxSeqNumber == 0)
+            {
+                maxSeqNumber = 63;
+            }
+            if (options.MaxSeqNumber < 0 || options.MaxSeqNumber > maxSeqNumber)
+            {
+                throw new ArgumentException("MaxSeqNumber error. (range:[1, " + maxSeqNumber + "]");
+            }
+
+            // 6.MinSeqNumber
+            if (options.MinSeqNumber < 5 || options.MinSeqNumber > maxSeqNumber)
+            {
+                throw new ArgumentException("MinSeqNumber error. (range:[5, " + maxSeqNumber + "]");
+            }
+
+            // 7.TopOverCostCount
+            if (options.TopOverCostCount < 0 || options.TopOverCostCount > 10000)
+            {
+                throw new ArgumentException("TopOverCostCount error. (range:[0, 10000]");
+            }
+
+            switch (options.Method)
+            {
+                case 2:
+                    _SnowWorker = new SnowWorkerM2(options);
+                    break;
+                default:
+                    if (options.DataCenterIdBitLength == 0 && options.TimestampType == 0)
+                    {
+                        _SnowWorker = new SnowWorkerM1(options);
+                    }
+                    else
+                    {
+                        _SnowWorker = new SnowWorkerM3(options);
+                    }
+                    break;
+            }
+
+            if (options.Method != 2)
+            {
+                Thread.Sleep(500);
+            }
+        }
+
+
+        public long NewLong()
+        {
+            return _SnowWorker.NextId();
+        }
+    }
+}
