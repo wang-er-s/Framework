4feeccc5084f0d872d927c9b42caadbe0cdb3633
王二 <18530117331@163.com>
2023-10-19 00:01:27 +0800

    行为树修改为异步

---
+++ Runtime/Asynchronous/AsyncResultHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Framework
+{
+    public static class AsyncResultHelper
+    {
+        public static IAsyncResult WaitAny(RecyclableList<IAsyncResult> results)
+        {
+            AsyncResult asyncResult = AsyncResult.Create(isFromPool: true);
+            foreach (IAsyncResult result in results)
+            {
+                result.Callbackable().OnCallback((r) =>
+                {
+                    asyncResult.SetResult();
+                });
+            }
+            return asyncResult;
+        }
+        
+        public static IAsyncResult WaitAll(params IAsyncResult[] results)
+        {
+            MulAsyncResult mulProgressResult = MulAsyncResult.Create(allProgress: results);
+            AsyncResult asyncResult = AsyncResult.Create();
+            return asyncResult;
+        } 
+    }
+}
